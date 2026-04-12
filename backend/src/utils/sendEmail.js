7aeea0aa31a3e10e5089650ebcf6ce0161564916
Luis Enrique Elías Gómez <149125562+luishi22@users.noreply.github.com>
2026-04-11 20:41:24 -0600

    fix email

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -3,7 +3,9 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (options) => {
   // 1. Crear el "Transportador" (El camión de correos)
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465, // El puerto seguro oficial de Google
+    secure: true, // Obliga a usar una conexión encriptada SSL
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
